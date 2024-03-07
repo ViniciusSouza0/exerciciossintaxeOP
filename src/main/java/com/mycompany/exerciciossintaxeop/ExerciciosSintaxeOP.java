d9a53f9be492608f92144b68b3ee1b167cedfda2
aluno <vinicius.s.silveira.2015@gmail.com>
2024-03-07 19:29:06 -0300

    Novo2

---
+++ src/main/java/com/mycompany/exerciciossintaxeop/ExerciciosSintaxeOP.java
@@ -0,0 +1,21 @@
+package com.mycompany.exerciciossintaxeop;
+
+public class ExerciciosSintaxeOP {
+
+    public static void main(String[] args) {
+
+int x =10;
+
+System.out.println((x += 5) *2 );
+        
+        
+int numero = 20;       
+
+System.out.println(numero += 1 );  
+        
+int y = 4;
+        
+System.out.println(y *= 2);
+
+    }
+}
